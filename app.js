9f8a6b1e510bd08514758560c21df93027690645
Ashen <ashen9834@gmail.com>
2022-03-19 01:34:43 +0530

    fixed bug(replaced arr[] with sum)

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const subArray = (arr) => {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
-        if (sum == 0 || map[arr[i]]) {
+        if (sum == 0 || map[sum]) {
             return console.log('yes');
         }
-        map[arr[i]] = true;
+        map[sum] = true;
     }
     return console.log('false');
 }
